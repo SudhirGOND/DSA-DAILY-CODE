5eec24aff89ba5f572f9626e91b7c182cfad2345
SudhirGOND <sudhirgonddhoom786@gmail.com>
2026-05-29 11:00:50 +0530

    Written the code of the Anagram

--- HASHMAP/src/Anagram.java
+++ HASHMAP/src/Anagram.java
@@ -1,6 +1,29 @@
+import java.util.HashMap;
+
 public class Anagram {
+    public static boolean AnagramValid(String s, String t) {
+        s = "silent";
+        t = "listen";
+        if (s.length() != t.length()) return false;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+        }
+
+        for (char ch : t.toCharArray()) {
+            if (!map.containsKey(ch)) return false; ////  wo element hi nhi to not anagram = false
+
+            int freq = map.get(ch);
+            if (freq == 0) return false;
+
+            map.put(ch, freq - 1);
 
 
+        }
+
+        return true;
+    }
 
 
 }
