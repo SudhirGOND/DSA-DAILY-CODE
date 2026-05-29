cd57e7f4d1eee5465d48b464c679688d4f8c632e
SudhirGOND <sudhirgonddhoom786@gmail.com>
2026-05-29 11:19:09 +0530

    Written the code of the Anagram

--- HASHMAP/src/Anagram.java
+++ HASHMAP/src/Anagram.java
@@ -1,29 +1,67 @@
+//import java.util.HashMap;
+//
+//public class Anagram {
+//    public static boolean AnagramValid(String s, String t) {
+//        s = "silent";
+//        t = "listen";
+//        if (s.length() != t.length()) return false;
+//        HashMap<Character, Integer> map = new HashMap<>();
+//        for (char ch : s.toCharArray()) {
+//            map.put(ch, map.getOrDefault(ch, 0) + 1);
+//
+//        }
+//
+//        for (char ch : t.toCharArray()) {
+//            if (!map.containsKey(ch)) return false; ////  wo element hi nhi to not anagram = false
+//
+//            int freq = map.get(ch);
+//            if (freq == 0) return false;
+//
+//            map.put(ch, freq - 1);
+//
+//        }
+//
+//        return true;
+//    }
+//
+//
+//}
+
+
 import java.util.HashMap;
 
 public class Anagram {
-    public static boolean AnagramValid(String s, String t) {
-        s = "silent";
-        t = "listen";
-        if (s.length() != t.length()) return false;
+
+    public static boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
         HashMap<Character, Integer> map = new HashMap<>();
+
         for (char ch : s.toCharArray()) {
             map.put(ch, map.getOrDefault(ch, 0) + 1);
-
         }
 
         for (char ch : t.toCharArray()) {
-            if (!map.containsKey(ch)) return false; ////  wo element hi nhi to not anagram = false
 
-            int freq = map.get(ch);
-            if (freq == 0) return false;
+            if (!map.containsKey(ch)) {
+                return false;
+            }
+            int freq  =  map.get(ch);
+            map.put(ch,freq- 1);/// remove the elements
 
-            map.put(ch, freq - 1);
-
-
+            if (freq == 0) {
+                map.remove(ch); /// remove the element from the map
+            }
         }
 
-        return true;
+        return map.isEmpty();
     }
 
+    public static void main(String[] args) {
 
+        System.out.println(isAnagram("silent", "listen"));
+    }
 }
