185f68215b71e209d283716e1d3fbe6d19c7ab37
SudhirGOND <sudhirgonddhoom786@gmail.com>
2026-06-05 19:55:32 +0530

    the fibonacci number usoing the DP

--- DYNAMIC_PROGRAMMING/src/Fibonacci.java
+++ DYNAMIC_PROGRAMMING/src/Fibonacci.java
@@ -14,11 +14,27 @@ public class Fibonacci {
 
     }
 
+    /// / using the tabulation method
+
+
+    public static int fibo2(int n) {
+
+
+        if (n <= 1) return n;
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
 
     public static void main(String[] args) {
-        int n = 10;
+        int n = 5;
         dp = new int[n + 1]; /// the size of the dp willbe the n +1
-        System.out.println(fibo(n));
+        System.out.println(fibo2(n));
 
 
     }
