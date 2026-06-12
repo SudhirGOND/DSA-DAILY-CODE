b8ccf6220994a592a4ae3a92758618368468f435
SudhirGOND <sudhirgonddhoom786@gmail.com>
2026-06-12 20:29:36 +0530

    modified

--- DYNAMIC_PROGRAMMING/src/MINCOSTCLIMBING.java
+++ DYNAMIC_PROGRAMMING/src/MINCOSTCLIMBING.java
@@ -20,18 +20,33 @@ public class MINCOSTCLIMBING {
     }
 
     /// tabulation of this
+    public int minpay(int[] cost) {
+        int n = cost.length;
+
+        int[] dp = new int[n];
+
+        dp[0] = cost[0];/// dp ke first index prr 1 element dalenge
+        dp[1] = cost[1];/// dp ke second place per 2 element
+        for (int i = 2; i < n; i++) {
+            dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]); //// last 2 mien se sbse kamm + curr cost
+        }
+
+        return Math.min(dp[n - 1], dp[n - 2]);/// last 2 mein se sbse kamm ans
+    }
+
+
+    /// space optimized
     ///
-    public int minpay(int[] cost, int i ) {
-        if (i >= cost.length)
-            return 0;
-        int[] sp =  new int[3];
+    public static int mincost_space_optimized(int[] cost) {
+        int prev2 = cost[0];
+        int prev1 = cost[1];
 
-        int take = minPay(cost, i + 2);
-        int jump = minPay(cost, i + 1); ////// House_robber(step->totally skipped) but in this(add the i+1 cost also)
-
-        int ans = cost[i] + Math.min(take, jump);
-        dp[i] = ans;
-        return ans;
+        for (int i = 2; i < cost.length; i++) {
+            int curr = cost[i] + Math.min(prev1, prev2);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return Math.min(prev1, prev2);
     }
 
     public int minCostClimbingStairs(int[] cost) {
