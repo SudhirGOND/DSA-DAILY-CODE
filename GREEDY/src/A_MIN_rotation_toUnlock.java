bc742cc357eda8151e76b72c3b78b9c165d4af8c
SudhirGOND <sudhirgonddhoom786@gmail.com>
2026-07-04 22:17:17 +0530

    Minimum rotation to Unlock the lock

---
+++ GREEDY/src/A_MIN_rotation_toUnlock.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class A_MIN_rotation_toUnlock {
+    public static void main(String[] args) {
+
+
+        int random = 223;
+        int password = 3394;
+        int ans = 0;
+
+        while (random > 0 || password > 0) {
+
+            int first = random % 10;
+            int second = password % 10;
+
+            random /= 10;
+            password /= 10;
+            int abs = Math.abs(first - second);
+
+            ans = Math.min(abs, 10 - abs);
+
+        }
+
+        System.out.println(ans);
+
+    }
+}
