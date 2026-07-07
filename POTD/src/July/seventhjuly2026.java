bccc666306c8d4956f1a041f8ce3c5e3e4abde15
SudhirGOND <sudhirgonddhoom786@gmail.com>
2026-07-07 18:39:36 +0530

    potd of the 7th july

---
+++ POTD/src/July/seventhjuly2026.java
@@ -0,0 +1,27 @@
+package July;
+
+public class seventhjuly2026 {
+    public static void main(String[] args) {
+        sumAndMultiply(204500050);
+    }
+
+    public static long sumAndMultiply(int n) {
+
+        long x = 0;
+        long sum = 0;
+        int placevalue = 1; ///  trick
+        while (n > 0) {
+            int last = n % 10;
+            if (last != 0) {
+                sum += last; // calculate the sum
+                x = x + last * placevalue; //  making of the x
+                placevalue *= 10;
+            }
+            n /= 10;
+
+        }
+        return (long) sum * x;
+    }
+}
+
+
