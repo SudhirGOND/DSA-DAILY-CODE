575fae1d82a9d4b68405f68bb940d3829a2b1a96
SudhirGOND <sudhirgonddhoom786@gmail.com>
2026-07-12 17:43:16 +0530

    POTD OF THE 12TH JULY 2026

---
+++ POTD/src/July/twelvethJuly2026.java
@@ -0,0 +1,33 @@
+package July;
+
+import java.util.*;
+
+public class twelvethJuly2026 {
+
+
+    public int[] arrayRankTransform(int[] arr) {
+
+        int[] nums = Arrays.copyOf(arr, arr.length);
+
+        Arrays.sort(nums);
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int rank = 1;
+
+        for (int num : nums) {
+            if (!map.containsKey(num)) {
+                map.put(num, rank++);
+            }
+        }
+
+        int[] ans = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            ans[i] = map.get(arr[i]);
+        }
+
+        return ans;
+    }
+}
+
