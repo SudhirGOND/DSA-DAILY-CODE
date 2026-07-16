c25026f8d6ee7a3442ae1ef6438803b8c75f1462
SudhirGOND <sudhirgonddhoom786@gmail.com>
2026-07-17 01:12:16 +0530

    no of islands

---
+++ GRAPH/src/D_NoOFIslands.java
@@ -0,0 +1,87 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class D_NoOFIslands {
+
+
+    class Pair {
+
+        int row;
+        int col;
+
+        Pair(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+    }
+
+
+    public int numIslands(char[][] grid) {
+
+        int m = grid.length;
+        int n = grid[0].length;
+        boolean visited[][] = new boolean[m][n];
+
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    bfst(i, j, visited, grid);
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private void bfst(int i, int j, boolean[][] visited, char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        Queue<Pair> q = new LinkedList<>();
+
+        q.add(new Pair(i, j));
+
+        while (!q.isEmpty()) {
+            Pair front = q.poll();
+
+            int row = front.row, col = front.col;
+            //// for the top   check
+            if (row > 0) {
+                if (grid[row - 1][col] == '1' && !visited[row - 1][col]) {
+
+                    q.add(new Pair(row - 1, col));
+                    visited[row - 1][col] = true;
+                }
+            }
+
+            /// for the right
+            if (col < n) {
+                if (grid[row][col + 1] == '1' && !visited[row][col + 1]) {
+
+                    q.add(new Pair(row, col + 1));
+                    visited[row][col + 1] = true;
+                }
+            }
+
+            // for the left check
+            if (col > 0) {
+                if (grid[row][col - 1] == '1' && !visited[row][col - 1]) {
+
+                    q.add(new Pair(row, col - 1));
+                    visited[row][col - 1] = true;
+                }
+            }
+            //// for the down
+            if (row < m) {
+                if (grid[row + 1][col] == '1' && !visited[row + 1][col]) {
+
+                    q.add(new Pair(row + 1, col));
+                    visited[row + 1][col] = true;
+                }
+            }
+        }
+    }
+
+
+}
