6c0639e3a9837b9e4ba41bff3d5fbbe48673b8ba
SudhirGOND <sudhirgonddhoom786@gmail.com>
2026-07-18 17:32:34 +0530

    no of islands DFS

--- GRAPH/src/D_NoOFIslands.java
+++ GRAPH/src/D_NoOFIslands.java
@@ -84,4 +84,60 @@ public class D_NoOFIslands {
     }
 
 
+
+
+    //// fromn here the code of the dfs
+    public int numIslandsdfs(char[][] grid) {
+
+        int m = grid.length;
+        int n = grid[0].length;
+        boolean visited[][] = new boolean[m][n];
+
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    dfs(i, j, visited, grid);
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private void dfs(int i, int j, boolean[][] visited, char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int row = i;
+        int col = j;
+
+        // Top
+        if (row > 0 && grid[row - 1][col] == '1' && !visited[row - 1][col]) {
+            visited[row - 1][col] = true;
+            dfs(row - 1, col, visited, grid);
+        }
+
+        // Right
+        if (col < n - 1 && grid[row][col + 1] == '1' && !visited[row][col + 1]) {
+            visited[row][col + 1] = true;
+            dfs(row, col + 1, visited, grid);
+
+        }
+
+        // Left
+        if (col > 0 && grid[row][col - 1] == '1' && !visited[row][col - 1]) {
+            visited[row][col - 1] = true;
+            dfs(row, col - 1, visited, grid);
+        }
+
+        // Down
+        if (row < m - 1 && grid[row + 1][col] == '1' && !visited[row + 1][col]) {
+            visited[row + 1][col] = true;
+            dfs(row + 1, col, visited, grid);
+        }
+    }
+
+
+
 }
