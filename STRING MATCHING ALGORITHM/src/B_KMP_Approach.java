b51076fb38293011058e6e690c07b9fa0907d3ce
SudhirGOND <sudhirgonddhoom786@gmail.com>
2026-09-14 17:47:45 +0530

    intuition added

--- STRING MATCHING ALGORITHM/src/B_KMP_Approach.java
+++ STRING MATCHING ALGORITHM/src/B_KMP_Approach.java
@@ -1,3 +1,22 @@
+/**
+ 1. Compare the string and pattern character by character.
+ 2. If characters match → move both pointers forward.
+ 3. If a mismatch occurs → don’t start over from the beginning.
+ 4. Use the LPS array to know how far the pattern can safely jump.
+ 5. LPS tells us the longest prefix that is also a suffix.
+ 6. This avoids rechecking characters that have already matched.
+ 7. Therefore, KMP searches in O(n + m) time.
+
+//LPS ARRAY KO FILL KAISE KARE
+ LPS ka matlab: Longest Proper Prefix which is also Suffix.
+ Har position par hum check karte hain: “Ab tak ke pattern mein starting ka kitna part ending mein bhi same hai?”
+ Jo maximum matching length milti hai, wahi LPS value hai.
+ Iska use KMP mein mismatch ke baad pattern ko unnecessarily restart hone se bachane ke liye hota hai.
+ Agar LPS[i] = k, iska matlab: first k characters = last k characters.
+**/
+
+
+
 public class B_KMP_Approach {
 
     public static void main(String[] args) {
