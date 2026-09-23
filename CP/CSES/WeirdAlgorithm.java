acb850cd7a2102409591f5231778a7349629daa0
SudhirGOND <sudhirgonddhoom786@gmail.com>
2026-09-23 22:54:24 +0530

    moved top CSES

---
+++ CP/CSES/WeirdAlgorithm.java
@@ -0,0 +1,46 @@
+/**
+ * Consider an algorithm that takes as input a positive integer n. If n is even, the algorithm divides it by two, and if n is odd, the algorithm multiplies it by three and adds one. The algorithm repeats this, until n is one. For example, the sequence for n=3 is as follows:
+ * $$ 3 \rightarrow 10 \rightarrow 5 \rightarrow 16 \rightarrow 8 \rightarrow 4 \rightarrow 2 \rightarrow 1$$
+ * Your task is to simulate the execution of the algorithm for a given value of n.
+ * Input
+ * The only input line contains an integer n.
+ * Output
+ * Print a line that contains all values of n during the algorithm.
+ * Constraints
+ * <p>
+ * 1 \le n \le 10^6
+ * <p>
+ * Example
+ * Input:
+ * 3
+ * <p>
+ * Output:
+ * 3 10 5 16 8 4 2 1
+ **/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class WeirdAlgorithm {
+
+
+    public static void main(String[] args) {
+//
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        System.out.print(n + " ");
+//        while (n != 1) {
+//            System.out.print(n + " ");
+//            if (n % 2 == 0) n /= 2;
+//            else {
+//                n *= 3;
+//                n++;
+//            }
+
+//
+//        }
+
+
+    }
+}
